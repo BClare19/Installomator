5ef68f57911d2aa6d1c72cd24dcd0ba73d7645f2
BClare19 <92439456+BClare19@users.noreply.github.com>
2022-05-16 14:51:21 +0100

    Create 1password8.sh
    Installs 1Password 8 the newest version 2022
    Does not uninstall 1Password 7

---
+++ fragments/labels/1password8.sh
@@ -0,0 +1,11 @@
+1password8)
+	name="1Password"
+	type="zip"
+	if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://downloads.1password.com/mac/1Password-latest-aarch64.zip"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://downloads.1password.com/mac/1Password-latest-x86_64.zip"
+    fi
+    expectedTeamID="2BUA8C4S2C"
+    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password (Safari)" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
+    ;;  
